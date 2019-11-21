48e7e7b02d212c3a0a663e56dc35e1e4b19d07fe
Eduardo Oviedo Blanco <eduardo.oviedo@gmail.com>
2019-11-21 16:58:41 -0600

    First ghaphql hello world

--- server.js
+++ server.js
@@ -3,15 +3,19 @@ var { graphql, buildSchema } = require('graphql');
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
  type Query {
-   hello: String
+   hello: String,
+   bye: String
  }
 `);
  
 // The root provides a resolver function for each API endpoint
 var root = {
- hello: () => {
-   return 'Hello world!';
- },
+  hello: () => {
+    return 'Hello world!';
+  },
+  bye: () => {
+    return 'adios!';
+  },
 };
  
 // Run the GraphQL query '{ hello }' and print out the response
